05818d91e15fa7d47f96fb72ea877360ef1b5032
sevilay bayatlı <sevilaybayatli@gmail.com>
2018-11-18 02:30:26 +0300

    Update RuleParser.h

--- src/RuleParser.h
+++ src/RuleParser.h
@@ -1,8 +1,14 @@
-
+/*
+ * ruleParser.h
+ *
+ *  Created on: Apr 25, 2018
+ *      Author: aboelhamd
+ */
 
 #ifndef SRC_RULEPARSER_H_
 #define SRC_RULEPARSER_H_
 
+//#include "../pugixml/pugixml.hpp"
 #include "pugixml.hpp"
 
 using namespace std;
