b46e8cd04a383d60f22704c1f382c23e7f368c57
sevilay bayatlı <sevilaybayatli@gmail.com>
2018-11-18 18:12:57 +0300

    Add files via upload

---
+++ rules-applier/CLExec.h
@@ -0,0 +1,74 @@
+/*
+ * CLExec.h
+ *
+ *  Created on: Jun 21, 2018
+ *      Author: aboelhamd
+ */
+
+#ifndef SRC_CLEXEC_H_
+#define SRC_CLEXEC_H_
+
+#include <string>
+#include <vector>
+
+//#include "../pugixml/pugixml.hpp"
+#include "pugixml.hpp"
+
+using namespace std;
+using namespace pugi;
+
+class CLExec
+{
+public:
+
+  static void
+  segmenter (string inFilePath, string outFilePath);
+
+  static void
+  lextor (string inFilePath, string outFilePath);
+
+  static void
+  biltrans (string inFilePath, string outFilePath);
+
+  static void
+  interchunk (string inFilePath, string outFilePath);
+
+  static void
+  postchunk (string inFilePath, string outFilePath);
+
+  static void
+  transfer (string inFilePath, string outFilePath);
+
+  static void
+  assignWeights (string inFilePath, string outFilePath);
+
+  static vector<string>
+  getFilesInDir (string dir);
+
+//  static void
+//  runYasmet ();
+
+  static map<string, map<string, vector<float> > >
+  loadYasmetModels (string modelsDest);
+
+  static void
+  handleDatasets ();
+
+  static string
+  toLowerCase (string word, string localeId);
+
+  static void
+  beamSearch (
+      vector<pair<vector<unsigned>, float> > *beamTree,
+      unsigned beam,
+      vector<string> slTokens,
+      vector<pair<pair<unsigned, unsigned>, pair<unsigned, vector<vector<xml_node> > > > > ambigInfo,
+      map<string, map<string, vector<float> > > classesWeights, string localeId);
+
+  static void
+  getTransInds (vector<pair<unsigned, float> > *Translations,
+		vector<pair<vector<unsigned>, float> > beamTree,
+		vector<vector<unsigned> > weigInds);
+};
+
+#endif /* SRC_CLEXEC_H_ */
