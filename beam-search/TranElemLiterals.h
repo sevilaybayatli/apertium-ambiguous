a55b48dbd8f2a414e6b40eec34a2c77ace05ee47
sevilay bayatlı <sevilaybayatli@gmail.com>
2018-11-18 18:42:25 +0300

    Add files via upload

---
+++ beam-search/TranElemLiterals.h
@@ -0,0 +1,65 @@
+/*
+ * TranElemLiterals.h
+ *
+ *  Created on: May 5, 2018
+ *      Author: aboelhamd
+ */
+
+#ifndef SRC_TRANELEMLITERALS_H_
+#define SRC_TRANELEMLITERALS_H_
+
+namespace elem
+{
+
+  const static char* ACTION = "action";
+  const static char* LET = "let";
+  const static char* OUT = "out";
+  const static char* CHOOSE = "choose";
+  const static char* CALL_MACRO = "call-macro";
+  const static char* WITH_PARAM = "with-param";
+  const static char* CLIP = "clip";
+  const static char* LIT_TAG = "lit-tag";
+  const static char* LIT = "lit";
+  const static char* CONCAT = "concat";
+  const static char* CHUNK = "chunk";
+  const static char* MLU = "mlu";
+  const static char* LU = "lu";
+  const static char* B = "b";
+  const static char* N = "n";
+  const static char* V = "v";
+  const static char* TAGS = "tags";
+  const static char* TAG = "tag";
+  const static char* WHEN = "when";
+  const static char* TEST = "test";
+  const static char* OR = "or";
+  const static char* AND = "and";
+  const static char* EQUAL = "equal";
+  const static char* RULE = "rule";
+  const static char* ID = "id";
+  const static char* PATTERN = "pattern";
+  const static char* PATTERN_ITEM = "pattern-item";
+  const static char* SECTION_RULES = "section-rules";
+  const static char* SECTION_DEF_MACROS = "section-def-macros";
+  const static char* DEF_MACRO = "def-macro";
+  const static char* SECTION_DEF_ATTRS = "section-def-attrs";
+  const static char* DEF_ATTR = "def-attr";
+  const static char* ATTR_ITEM = "attr-item";
+  const static char* SECTION_DEF_CATS = "section-def-cats";
+  const static char* DEF_CAT = "def-cat";
+  const static char* CAT_ITEM = "cat-item";
+  const static char* SECTION_DEF_VARS = "section-def-vars";
+  const static char* DEF_VARS = "def-var";
+  const static char* POS = "pos";
+  const static char* SIDE = "side";
+  const static char* PART = "part";
+  const static char* SL = "sl";
+  const static char* TL = "tl";
+  const static char* WHOLE = "whole";
+  const static char* ATTR = "attr";
+  const static char* LEM = "lem";
+  const static char* NAME = "name";
+  const static char* DATASETS = "datasets";
+  const static char* MODELS = "models";
+}
+
+#endif /* SRC_TRANELEMLITERALS_H_ */
