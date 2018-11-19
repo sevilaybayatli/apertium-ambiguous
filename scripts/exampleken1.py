0b51c604c947d8c477f0ed1dedde7018a8a1ce49
sevilay bayatlı <sevilaybayatli@gmail.com>
2018-11-19 17:16:00 +0300

    Update exampleken1.py

--- scripts/exampleken1.py
+++ scripts/exampleken1.py
@@ -10,7 +10,7 @@ text = sys.stdin.read()
 #print("Loding model started")
 
 # Load the language model
-LM = os.path.join(os.path.dirname('/home/sevilay/apertium-kaz-tur-mt/scripts/training.klm'), '..', 'scripts', 'training.klm')
+LM = os.path.join(os.path.dirname('/home/sevilay/apertium-kaz-tur-mt/scripts/text.binary'), '..', 'scripts', 'text.binary')
 model = kenlm.LanguageModel(LM)
 
 #print("Loding model finished")
