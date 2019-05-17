735a1d1ddcc5714bf04523d4b37e56829af204d7
sevilay bayatlı <sevilaybayatli@gmail.com>
2019-05-17 16:22:26 +0300

    Update sentenceTokenizer.rb

--- scripts/sentenceTokenizer.rb
+++ scripts/sentenceTokenizer.rb
@@ -1,12 +1,17 @@
 require 'pragmatic_segmenter'
 
+if (ARGV.length < 3)
+  puts "\nUsage : ruby2.3 sentenceTokenizer.rb 639-1ISOlangCode textFilePath sentencesFilePath"
+  exit
+end
+
 File.open(ARGV[1]).each do |line1|
 	line1.delete! ('\\\(\)\[\]\{\}\<\>\|\$\/\'\"')
-    ps = PragmaticSegmenter::Segmenter.new(text: line1, language: ARGV[0], doc_type: 'txt')
-    sentences = ps.segment
+  ps = PragmaticSegmenter::Segmenter.new(text: line1, language: ARGV[0], doc_type: 'txt')
+  sentences = ps.segment
     
-    File.open(ARGV[2], "a") do |line2|
-        sentences.each { |sentence| line2.puts sentence }
-    end
+  File.open(ARGV[2], "a") do |line2|
+      sentences.each { |sentence| line2.puts sentence }  
+  end
 end
 
